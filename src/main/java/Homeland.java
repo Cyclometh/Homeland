ab91c83e4c9c6376f2736d06f3b85f92a67ca8ca
Cyclometh <coreysnow@gmail.com>
2016-10-08 23:22:02 -0700

    Creating packages; building project structure and organizing git repo.

--- src/main/java/Homeland.java
+++
@@ -1,7 +0,0 @@
-import org.spongepowered.api.plugin.Plugin;
-
-@Plugin(id="homeland", name= "Homeland", version="0.1-ALPHA")
-
-public class Homeland {
-
-}
---
+++ src/main/java/com/cyclometh/spongepowered/homeland/Homeland.java
@@ -0,0 +1,9 @@
+package com.cyclometh.spongepowered.homeland;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id="homeland", name= "com.cyclometh.spongepowered.homeland.Homeland", version="0.1-ALPHA")
+
+public class Homeland {
+
+}
